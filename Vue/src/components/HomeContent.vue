9cdf486c71bdf4e6d73a276999ad0a6a51efa34a
Diana Gromova <16adianay@gmail.com>
2024-05-03 21:21:54 +0400

    Vue example

--- Vue/src/components/HomeContent.vue
+++ Vue/src/components/HomeContent.vue
@@ -1,22 +1,40 @@
-<script setup lang="ts">
-import { computed, ref } from "vue";
-
-import "devextreme/dist/css/dx.material.blue.light.compact.css";
-import DxButton from "devextreme-vue/button";
-
-const props = defineProps({
-  text: String,
-});
-const count = ref(0);
-const buttonText = computed<String>(
-  () => `Click ${props.text}: ${count.value}`
-);
-function clickHandler() {
-  count.value += 1;
-}
-</script>
 <template>
   <div>
-    <DxButton :text="buttonText" @click="clickHandler"></DxButton>
+    <DxHtmlEditor
+      :height="500"
+      :value="markup"
+      @initialized="onInitialized"
+    >
+      <DxToolbar>
+        <DxItem name="undo"/>
+        <DxItem name="redo"/>
+        <DxItem name="separator"/>
+        <DxItem name="clear"/>
+      </DxToolbar>
+    </DxHtmlEditor>
   </div>
 </template>
+
+<script setup lang="ts">
+import { markup } from '@/data';
+import 'devextreme/dist/css/dx.material.blue.light.compact.css';
+import {
+  DxHtmlEditor,
+  DxToolbar,
+  DxItem,
+  DxHtmlEditorTypes
+} from 'devextreme-vue/html-editor';
+
+function onInitialized(e: DxHtmlEditorTypes.InitializedEvent): void {
+  const Uploader = e.component?.get('modules/uploader');
+  class DisabledUploader extends Uploader {
+    constructor(quill: any, options: any) {
+    // eslint-disable-next-line spellcheck/spell-checker
+      super(quill, { ...options, mimetypes: [] });
+    // empty array to prevent any image type from being pasted
+    }
+  }
+  e.component?.register({ 'modules/uploader': DisabledUploader });
+}
+</script>
+
