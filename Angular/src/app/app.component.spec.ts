8cb208221bf555cf5e9b1a983e6f0ca2a394ed2f
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2025-10-05 13:45:55 +0300

    Fix issues

--- Angular/src/app/app.component.spec.ts
+++ Angular/src/app/app.component.spec.ts
@@ -1,15 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { DxHtmlEditorModule } from 'devextreme-angular/ui/html-editor';
 import { AppComponent } from './app.component';
+import { Service } from './app.service';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
+        DxHtmlEditorModule,
       ],
       declarations: [
         AppComponent,
+      ],
+      providers: [
+        Service,
       ],
     }).compileComponents();
   });
@@ -20,16 +26,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it('should have as title \'angular-test\'', () => {
+  it('should have valueContent property', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-test');
+    expect(app.valueContent).toBeDefined();
   });
 
-  it('should render title', () => {
+  it('should render instruction text', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-test app is running!');
+    expect(compiled.querySelector('p')?.textContent).toContain('Try dropping or pasting an image to the editor.');
   });
 });
